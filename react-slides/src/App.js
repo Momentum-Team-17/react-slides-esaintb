42eae4a49fee63a36a9d94dff118c9bba444c9d3
esantb <e.saint.bou@gmail.com>
2023-03-16 16:42:17 -0500

    content showing up, buttons working: next, back, restart

--- react-slides/src/App.js
+++ react-slides/src/App.js
@@ -26,7 +26,7 @@ function Gallery() {
   }
 
   function handleRestartClick() {
-    setIndex(index === 0);
+    setIndex(0);
   }
 
   let data = filmData[index];
@@ -43,26 +43,14 @@ function Gallery() {
       </button>
 
       <h2>{data.title}</h2>
-      <h2>{data.original_title}</h2>
-      <h2>{data.release_date}</h2>
-      <h2>{data.description}</h2>
-      <div><img src={data.image} alt="cover" /></div>
-
-
-
-
-
-      {/* <h3>
-        ({index + 1} of {filmData.length})
-      </h3> */}
-      {/* <button onClick={handleMoreClick}>
-        {showMore ? 'Hide' : 'Show'} details
-    </button> */}
-      {/* {showMore && <p>{sculpture.description}</p>}
-    <img
-        src={sculpture.url}
-        alt={sculpture.alt} */}
-
+      <div className='juice'>
+        <div><img src={data.image} alt="cover" /></div>
+        <div>
+          <h2>{data.original_title}</h2>
+          <h2>{data.release_date}</h2>
+          <h2>{data.description}</h2>
+        </div>
+      </div>
     </>
   );
 }
